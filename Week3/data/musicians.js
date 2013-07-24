2d9dc885c9def1d091c7371cddb4ccd0cd8453a6
Russell Gaspard <russg@fullsail.edu>
2013-07-23 22:57:42 -0400

    begin fixes

---
+++ Week3/data/musicians.js
@@ -0,0 +1,37 @@
+[
+	{
+		"fname":	"Russ",	
+		"lname":	"Gaspard",	
+		"phone":	"321-439-0605",					
+		"instr":	"Guitar",	
+		"notes":	"Russ also plays fiddle, mandolin and harmonica."	
+	},
+	{
+		"fname":	"Dave",	
+		"lname":	"Cox",	
+		"phone":	"321-331-2791",		
+		"instr":	"Bass",	
+		"notes":	"Dave is not as interested in learning material but always ready to jam."	
+	},
+	{
+		"fname":	"Dave",	
+		"lname":	"Gibbs",	
+		"phone":	"407-529-8561",					
+		"instr":	"Keys",		
+		"notes":	"Hard to get Dave to commit to a gig but always worth it."	
+	},
+	{
+		"fname":	"John Paul",	
+		"lname":	"McCune",	
+		"phone":	"321-806-8618",				
+		"instr":	"Drums",		
+		"notes":	"Love this guy."	
+	},
+	{
+		"fname":	"Jenn",	
+		"lname":	"Willsey",	
+		"phone":	"407-252-4264",				
+		"instr":	"Vocals",			
+		"notes":	"Sings a fine lead but incredible harmony as well."	
+	}
+]
