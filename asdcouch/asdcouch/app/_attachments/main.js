de2c607fa09bf1a4a8e7f973ad430d2175a7cd83
Russell Gaspard <russg@fullsail.edu>
2013-08-01 23:39:29 -0400

    final

--- asdcouch/asdcouch/app/_attachments/main.js
+++ asdcouch/asdcouch/app/_attachments/main.js
@@ -1,55 +1,37 @@
 // Russell Gaspard
-// ASD 1307, Project 3
+// ASD 1307, Project 4
 // Mobile Development
 // Full Sail University
-
-
-
-
-//$('#home').on('pageinit', function(){
-//	$.couch.db("asdproject").view("bandmates/musicians", {
-//		success: function(data){
-//			console.log(data);
-//		}
-//	});
-//});
 
 	
 ///////////////  HOME ///////////////////////	
 
-var globalKey = 0;
-
 $('#home').on('pageinit', function(){
+
+
+//http://127.0.0.1:5984/asdproject/_design/app/_view/musicians
+
+//http://127.0.0.1:5984/asdproject/_design/bandmates/index.html
+
+var fnDisplayData = function(){
 
 	$.couch.db("asdproject").view("bandmates/musicians", {
 		success: function(data){
-			console.log("Russell's couch plugin...");	
-			console.log(data);
-		}
-	});
 
-//console.log("Here I am couch surfing!!!");
-//http://127.0.0.1:5984/asdproject/_design/app/_view/musicians
-
-//Import RESTful Couch data
-var fnImportCouch = function(){
-
-	$.ajax({
-		url      : '_view/musicians',
-		type     : 'GET',
-		dataType : 'json',
-		success  : function(data, status) {
-		
 			$('#count').text(data.total_rows);
 		
 			$.each(data.rows, function(index, musician){
+
+				var id = musician.value.id;	
+				var rev = musician.value.rev;			
 				var fname = musician.value.fname;
 				var lname = musician.value.lname;
 				var instr = musician.value.instr;
 				var phone = musician.value.phone;
 				var notes = musician.value.notes;		
 
-				var info = '<li><a href="#"><h3>' + fname + ' ' + lname + '</h3>' +
+				var info = '<li><a href="detail.html?id=' + id + '">' +
+				'<h3>' + fname + ' ' + lname + '</h3>' +
 				'<p>' + instr + '</p>' +
 				'<p>' + phone + '</p>' +
 				'<p>' + notes + '</p>' +				
@@ -59,175 +41,94 @@ var fnImportCouch = function(){
 				$('#list').append(info);
 			});
 			$('#list').listview('refresh');
-		}	
-	});
-};
-
-
-var fnDisplayData = function(){
-
-	var list = $('#list');
-	list.html('');
-	
-	var count = $('#count');
-
-	if(localStorage.length === 0){
-	
-			var info = '<li><h3>' + 'No Musician Data Recorded' + '</h3>,</li>'
-			//'<p>' + data.instr + '</p>' +
-			//'<p>' + data.phone + '</p>' +
-			//'<p>' + data.notes + '</p></li>' 	
-			list.append(info);
-			count.text('');
-			//$('#clear').css("display", "hidden");
-	}else{
-		//console.log('hello');
-		var list = $('#list');
-		list.html('');
-		
-		for(i=0, j=localStorage.length; i<j; i++){
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			var data = JSON.parse(value);
-			
-			var info = '<li><h3>' + data.fname + ' ' + data.lname + '</h3>' +
-			'<p>' + data.instr + '</p>' +
-			'<p>' + data.phone + '</p>' +
-			'<p>' + data.notes + '</p>' +
-			'<a href="#" class="edit" id="' + key + '">Edit</a> <a href="#" class="del" id="' + key + '">Delete</a>' + 
-			'<hr/></li>' 	
-			list.append(info);
 		}
-		count.text(i);
-		//$('#clear').css("display", "inline");
-	}
+	});
 }
 
 
-//Import JSON
-var fnImportJSON = function(){
-
-  
-	$.ajax({
-		url      : 'data/musicians.js',
-		type     : 'GET',
-		dataType : 'json',
-		success  : function(data, status) {
-			//console.log(data)
-				
-			//Loop through Musicians in JSON data & save to local storage
-			for(var i=0; i< data.length; i++){  
-				
-				//Make random number key for local storage
-				var randomID = Math.floor(Math.random()*100000);
-				
-				//Save into localStorage
-				localStorage.setItem(randomID, JSON.stringify(data[i]));
-				console.log(randomID + " " + JSON.stringify(data[i]));
-				
-				//OUTPUT TO CONSOLE
-				//console.log(data[i].fname)
-				//console.log(data[i].lname)	
-				//console.log(data[i].phone)
-				//console.log(data[i].instr)
-				//console.log(data[i].notes)
-			}
-				
-            //Report to user
-            //alert("JSON Musician Data Saved");
-            
-            //Show Data
-            fnDisplayData();
-
-		},
-		error  : function(error, parseerror) {
-			console.log(error)
-				
-            //Report to user
-            alert("Error importing JSON data");
-		}
-	});
-};
-
 $('#home').on('pageshow', function(){
-
-	//fnDisplayData();
-
+	fnDisplayData();
 });
 	
-//Import XML
-var fnImportXML = function(){
-  
-	$.ajax({
-		url      : 'data/musicians.xml',
-		type     : 'GET',
-		dataType : 'xml',
-		success  : function(data, status) {
-			
-			//Navigate to root element & then each child in XML data
-			//Looping through Musicians data & save to local storage
-					
-			$(data).children(0).children().each(function(){ 
-						
-				//Make random number key for local storage
-				var randomID = Math.floor(Math.random()*100000);
-						
-				//Create object to hold properties.
-				var entry = {};
-					
-				entry.fname		= $(this).find('fname').text();
-				entry.lname		= $(this).find('lname').text();
-				entry.phone		= $(this).find('phone').text();
-				entry.instr		= $(this).find('instr').text();
-				entry.notes		= $(this).find('notes').text();	
-								
-				//Save into localStorage
-				localStorage.setItem(randomID, JSON.stringify(entry));
-				
-				//console.log(randomID + " " + JSON.stringify(entry));
-									
-				//OUTPUT TO CONSOLE
-				//console.log($(this).find('fname').text());
-				//console.log($(this).find('lname').text());				
-				//console.log($(this).find('phone').text());	
-				//console.log($(this).find('instr').text());		
-				//console.log($(this).find('notes').text());	
-            });
-                	
-            //Report to user
-            //alert("XML Musician Data Saved");
-            
-            //Show Data
-            fnDisplayData();
 
-		},
-		error  : function(error, parseerror) {
-			console.log(error)
-				
-        	//Report to user
-        	alert("Error importing XML data");
+//$('#list').delegate('a.edit', 'click', fnEdit);
+//$('#list').delegate('a.del', 'click', fnDelete);
+
+});
+
+///////////////  DETAILS  ///////////////////////	
+
+var urlVars = function(urlData){
+	var urlParts = urlData.split('?');
+	var urlPairs = urlParts[1].split('&');
+	var urlValues = {};
+	for (var i in urlPairs){
+		var keyValue = urlPairs[i].split('=');
+		var key = decodeURIComponent(keyValue[0]);
+		var value = decodeURIComponent(keyValue[1]);
+		urlValues[key] = value;
+	}
+	return urlValues;
+};
+
+var fnDisplayDetail = function(choice){
+
+	$.couch.db("asdproject").view("bandmates/musicians", {
+		success: function(data){
+	
+			$.each(data.rows, function(index, musician){
+
+				if(choice === musician.value.id){
+					var id = musician.value.id;	
+					var rev = musician.value.rev;					
+					var fname = musician.value.fname;
+					var lname = musician.value.lname;
+					var instr = musician.value.instr;
+					var phone = musician.value.phone;
+					var notes = musician.value.notes;		
+
+					var info = '<li><h3>' + fname + ' ' + lname + '</h3>' +
+					'<li>' + instr + '<li>' +
+					'<li>' + phone + '</li>' +
+					'<li>' + notes + '</li>' +
+					'<input type="hidden" id="key" value="' +  id + '"/>' +
+					'<input type="hidden" id="rev" value="' +  rev + '"/>'
+									
+					//'<a href="#" class="edit" id="' + key + '">Edit</a> <a href="#" class="del" id="' + key + '">Delete</a>' + 		
+					$('#display').html(info);
+				};
+			});
 		}
 	});
-
-};	
-
-
-// Clear all data from local storage
-var fnClearData	= function (){
-	if(localStorage.length === 0){
-		alert("No musician data has been saved.");
-	}else{
-		localStorage.clear();
-    	fnDisplayData(); //Show Data
-    	//window.location.reload();
-	}
 }
 
 // Delete
+var fnVerify = function (){
+	$('#verify').css("display","block;");
+	alert(id);
+
+	//localStorage.removeItem(this.id)
+    //fnDisplayData(); //Show Data
+};
+
+// Delete
 var fnDelete = function (){
-	localStorage.removeItem(this.id)
-    fnDisplayData(); //Show Data
-}
+	var id = $('#key').val();
+	var rev = $('#rev').val();
+	
+	var ask = confirm("Delete this entry?");
+	if (ask === true) {
+		$.couch.db('asdproject').removeDoc({
+			_id : id,
+			_rev : rev
+		},{
+			success: function(data){
+			alert('Deleted!');
+		}
+});
+  	} else {
+  	}
+};
 
 // Edit
 var fnEdit = function (){
@@ -235,20 +136,18 @@ var fnEdit = function (){
 	alert('Edit feature coming soon!');
 	//globalKey = this.id;
 	//$.mobile.changePage('#add');
-}
-	
+};
+
+$(document).on('pageinit', '#details', function(){
+	var url =  $(this).data("url");
+	var id = urlVars(url)["id"];
+	fnDisplayDetail(id);
+
+
 //Event binders	
-//$('#ijson').on('click', fnImportJSON);
-$('#ijson').on('click', fnImportCouch);
-//$('#ixml').on('click', fnImportXML);
-//$('#clear').on('click', fnClearData);
-
-
-$('#list').delegate('a.edit', 'click', fnEdit);
-$('#list').delegate('a.del', 'click', fnDelete);
+$('#deleteNav').on('click', fnDelete);
 
 });
-
 
 ///////////////  ADD  ///////////////////////	
 
@@ -263,22 +162,24 @@ var fnSaveData = function(){
 	//Create object to hold properties.
 	var entry = {};
 					
+	entry.type		= "musician";			
 	entry.fname		= $('#fname').val();
 	entry.lname		= $('#lname').val();
 	entry.phone		= $('#phone').val();
 	entry.instr		= $('#instr').find(":selected").val();
 	entry.notes		= $('#notes').val();	
-	
-	//OUTPUT TO CONSOLE
-	//console.log($('#fname').val());
-	//console.log($('#lname').val());				
-	//console.log($('#phone').val());	
-	//console.log($('#instr').find(":selected").val());		
-	//console.log($('#notes').val());
-	
-								
+							
 	//Save into localStorage
-	localStorage.setItem(randomID, JSON.stringify(entry));
+	//localStorage.setItem(randomID, JSON.stringify(entry));
+	
+	$.couch.db('asdproject').saveDoc(entry, {
+	success: function(){
+		alert('Musician has been saved!');
+		fnDisplayData();
+	}
+});
+	
+	
 	fnClearFields();
 }
 
--- asdcouch/asdcouch/app/views/musicians/map.js
+++ asdcouch/asdcouch/app/views/musicians/map.js
@@ -1,6 +1,8 @@
 function(doc) {
   if (doc.type==="musician") {
     emit(doc._id, {
+    	"id":doc._id,
+    	"rev":doc._rev,
     "fname": doc.fname,
     "lname": doc.lname,
     "instr": doc.instr,
